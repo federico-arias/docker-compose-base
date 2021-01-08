f5f71b9e4dfc25ca5601ff6f74f906c1c92c4704
Federico Arias <federicoariasr@gmail.com>
2021-01-08 11:22:03 -0300

    [add] mongo init script

--- .mongo/docker-entrypoint-initdb.d/init.js
+++ .mongo/docker-entrypoint-initdb.d/init.js
@@ -1,13 +1,27 @@
-db.auth('mongouser', 'mongopassword')
+db = db.getSiblingDB("admin");
 
+db.auth("mongouser", "mongopassword");
+
+// this is strange but necessary
+// https://docs.mongodb.com/manual/reference/method/db.getSiblingDB/#example
+db = db.getSiblingDB("test");
+
+/*
 db.createUser({
-  user: 'test-user',
-  pwd: 'test-password',
-  roles: [
-    {
-      role: 'root',
-      db: 'test',
-    },
-  ],
+	user: "test-user",
+	pwd: "test-password",
+	roles: [
+		{
+			role: "root",
+			db: "test"
+		}
+	]
 });
+*/
 
+db.createCollection("user.preferences");
+
+db.user.preferences.insert({
+	user_id: 1,
+	preferences: { web: { notifications: { whatsapp: true } } }
+});
